42af84d748a5669eeb6f8ce9fc88654faee1ea36
SogZiw <sogziw@foxmail.com>
2020-09-21 13:59:14 +0800

    fix

--- app/src/main/java/com/song/sakura/ui/web/WebViewActivity.kt
+++ app/src/main/java/com/song/sakura/ui/web/WebViewActivity.kt
@@ -1,16 +1,24 @@
 package com.song.sakura.ui.web
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.Activity
 import android.app.Application
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.net.http.SslError
+import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
 import android.webkit.*
+import android.webkit.WebChromeClient.FileChooserParams
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.hjq.permissions.OnPermission
+import com.hjq.permissions.Permission
+import com.hjq.permissions.XXPermissions
+import com.hjq.toast.ToastUtils
 import com.song.sakura.R
 import com.song.sakura.extension.checkAppInstalled
 import com.song.sakura.extension.openBrowser
@@ -18,6 +26,7 @@ import com.song.sakura.route.Router
 import com.song.sakura.ui.base.IBaseActivity
 import com.song.sakura.ui.base.IBaseViewModel
 import com.song.sakura.ui.share.ShareDialogFragment
+import com.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_webview.*
 
 @Route(path = Router.Main.webview)
@@ -60,9 +69,10 @@ class WebViewActivity : IBaseActivity<WebViewModel>() {
             javaScriptEnabled = true
             allowContentAccess = true
             databaseEnabled = true
-            /* 大部分网页需要自己保存一些数据,这个时候就的设置下面这个属性 */
+            // 本地 DOM 存储（解决加载某些网页出现白板现象）
             domStorageEnabled = true
             setAppCacheEnabled(true)
+            // 允许保存密码
             savePassword = false
             saveFormData = false
             /* 设置为使用webview推荐的窗口 */
@@ -70,9 +80,11 @@ class WebViewActivity : IBaseActivity<WebViewModel>() {
             /* 设置网页自适应屏幕大小 ---这个属性应该是跟上面一个属性一起用 */
             loadWithOverviewMode = true
             defaultTextEncodingName = "UTF-8"
-
+            // 加快网页加载完成的速度，等页面完成再加载图片
+            loadsImagesAutomatically = true
             /* 设置WebView是否可以由JavaScript自动打开窗口，默认为false，通常与JavaScript的window.open()配合使用。*/
             javaScriptCanOpenWindowsAutomatically = true
+            // 允许文件访问
             allowFileAccess = true
             /* HTML5的地理位置服务,设置为true,启用地理定位 */
             setGeolocationEnabled(true)
@@ -80,6 +92,10 @@ class WebViewActivity : IBaseActivity<WebViewModel>() {
             builtInZoomControls = false
             /* 提高网页渲染的优先级 */
 //            setRenderPriority(WebSettings.RenderPriority.HIGH)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // 解决 Android 5.0 上 WebView 默认不允许加载 Http 与 Https 混合内容
+                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            }
 
         }
 
@@ -111,11 +127,48 @@ class WebViewActivity : IBaseActivity<WebViewModel>() {
                 progress.progress = newProgress
             }
 
+            override fun onShowFileChooser(
+                mWebView: WebView?,
+                callback: ValueCallback<Array<Uri?>?>,
+                params: FileChooserParams?
+            ): Boolean {
+                val activity: Activity = this@WebViewActivity
+                if (activity is BaseActivity) {
+                    XXPermissions.with(activity)
+                        .permission(*Permission.Group.STORAGE)
+                        .request(object : OnPermission {
+                            override fun hasPermission(granted: List<String>, all: Boolean) {
+                                if (all && params != null) {
+                                    openSystemFileChooser(activity, callback, params)
+                                } else {
+                                    callback.onReceiveValue(null)
+                                }
+                            }
+
+                            override fun noPermission(denied: List<String>, quick: Boolean) {
+                                callback.onReceiveValue(null)
+                                if (quick) {
+                                    ToastUtils.show("授权失败，请手动授予权限")
+                                    XXPermissions.startPermissionActivity(activity, denied)
+                                } else {
+                                    ToastUtils.show("请先授予权限")
+                                }
+                            }
+                        })
+                }
+                return true
+            }
+
         }
         /* 同上,重写WebViewClient可以监听网页的跳转和资源加载等等... */
         webView.webViewClient = object : WebViewClient() {
 
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            @TargetApi(Build.VERSION_CODES.N)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return shouldOverrideUrlLoading(view, request?.url.toString())
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
                 if (url.startsWith("scheme:") || url.startsWith("scheme:")) {
                     url.openBrowser(this@WebViewActivity)
                     return true
@@ -146,11 +199,20 @@ class WebViewActivity : IBaseActivity<WebViewModel>() {
                 super.onPageFinished(view, url)
             }
 
-            override fun onReceivedSslError(
-                view: WebView?,
-                handler: SslErrorHandler?,
-                error: SslError?
-            ) {
+            /** 同名 API 兼容 */
+            @TargetApi(Build.VERSION_CODES.M)
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                if (request!!.isForMainFrame) {
+                    onReceivedError(view, error!!.errorCode, error.description.toString(), request.url.toString())
+                }
+            }
+
+            override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
+                // 注意一定要去除这行代码，否则设置无效。
+                //super.onReceivedSslError(view, handler, error)
+                // Android默认的处理方式
+                //handler?.cancel()
+                // 接受所有网站的证书
                 handler?.proceed()
             }
 
@@ -161,6 +223,46 @@ class WebViewActivity : IBaseActivity<WebViewModel>() {
             val uri = Uri.parse(url)
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
+        }
+    }
+
+
+    /**
+     * 打开系统文件选择器
+     */
+    private fun openSystemFileChooser(
+        activity: BaseActivity,
+        callback: ValueCallback<Array<Uri?>?>,
+        params: FileChooserParams
+    ) {
+        val intent = params.createIntent()
+        val mimeTypes = params.acceptTypes
+        if (!mimeTypes.isNullOrEmpty() && mimeTypes[0] != null && "" != mimeTypes[0]) {
+            // 设置要过滤的文件类型
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        }
+        // 设置是否是多选模式
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, params.mode == FileChooserParams.MODE_OPEN_MULTIPLE)
+        activity.startActivityForResult(Intent.createChooser(intent, params.title)) { resultCode, data ->
+            var uris: Array<Uri?>? = null
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                val uri = data.data
+                if (uri != null) {
+                    // 如果用户只选择了一个文件
+                    uris = arrayOf(uri)
+                } else {
+                    // 如果用户选择了多个文件
+                    val clipData = data.clipData
+                    if (clipData != null) {
+                        uris = arrayOfNulls(clipData.itemCount)
+                        for (i in 0 until clipData.itemCount) {
+                            uris[i] = clipData.getItemAt(i).uri
+                        }
+                    }
+                }
+            }
+            // 不管用户最后有没有选择文件，最后还是调用 onReceiveValue，如果没有调用就会导致网页再次上传无响应
+            callback.onReceiveValue(uris)
         }
     }
 
