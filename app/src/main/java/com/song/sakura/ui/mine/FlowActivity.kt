886894fc696476da93d184c7e8a1b98f32b32cf1
SogZiw <sogziw@foxmail.com>
2021-01-11 10:35:55 +0800

    add helper

--- app/src/main/java/com/song/sakura/ui/mine/FlowActivity.kt
+++ app/src/main/java/com/song/sakura/ui/mine/FlowActivity.kt
@@ -2,12 +2,14 @@ package com.song.sakura.ui.mine
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.PointF
 import android.os.Build
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewAnimationUtils
 import androidx.constraintlayout.helper.widget.Flow.*
+import androidx.constraintlayout.helper.widget.Layer
 import androidx.constraintlayout.widget.ConstraintHelper
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.song.sakura.R
@@ -86,6 +88,38 @@ class CircularRevealHelper @JvmOverloads constructor(
                 anim.duration = 3000
                 anim.start()
             }
+        }
+    }
+}
+
+class Flyin2Helper @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : Layer(context, attrs, defStyleAttr) {
+
+    override fun updatePostLayout(container: ConstraintLayout) {
+        super.updatePostLayout(container)
+        val centerPoint = PointF(((left + right) / 2).toFloat(), ((top + bottom) / 2).toFloat())
+
+        val animator = ValueAnimator.ofFloat(0f, 1f).setDuration(1000)
+        animator.addUpdateListener { animation ->
+            val animatedFraction = animation.animatedFraction
+            updateTranslation(centerPoint, animatedFraction, container)
+        }
+        animator.start()
+    }
+
+    private fun updateTranslation(centerPoint: PointF, animatedFraction: Float, container: ConstraintLayout) {
+        val views = getViews(container)
+        for (view in views) {
+
+            val viewCenterX = (view.left + view.right) / 2
+            val viewCenterY = (view.top + view.bottom) / 2
+
+            val startTranslationX = if (viewCenterX < centerPoint.x) -2000f else 2000f
+            val startTranslationY = if (viewCenterY < centerPoint.y) -2000f else 2000f
+
+            view.translationX = (1 - animatedFraction) * startTranslationX
+            view.translationY = (1 - animatedFraction) * startTranslationY
         }
     }
 }
